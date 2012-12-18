0e9123048b8b271e84a96bb9a17940d86bb3d513
Daniel Leung <daniel.leung@intel.com>
2012-12-17 16:44:02 -0800

    Adding firmware for Intel Wifi 1000, 2000 series
    Change-Id: I33720451c8183d4509ab3a94bbe009b6dfd643d4
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- iwlwifi/iwlwifi.mk
+++ iwlwifi/iwlwifi.mk
@@ -1,6 +1,11 @@
 # Wifi firmware
 PRODUCT_COPY_FILES += \
+        vendor/intel/iwlwifi/iwlwifi-100-5.ucode:system/etc/firmware/iwlwifi-100-5.ucode \
+        vendor/intel/iwlwifi/iwlwifi-105-6.ucode:system/etc/firmware/iwlwifi-105-6.ucode \
+        vendor/intel/iwlwifi/iwlwifi-135-6.ucode:system/etc/firmware/iwlwifi-135-6.ucode \
         vendor/intel/iwlwifi/iwlwifi-1000-5.ucode:system/etc/firmware/iwlwifi-1000-5.ucode \
+        vendor/intel/iwlwifi/iwlwifi-2000-6.ucode:system/etc/firmware/iwlwifi-2000-6.ucode \
+        vendor/intel/iwlwifi/iwlwifi-2030-6.ucode:system/etc/firmware/iwlwifi-2030-6.ucode \
         vendor/intel/iwlwifi/iwlwifi-6000g2a-5.ucode:system/etc/firmware/iwlwifi-6000g2a-5.ucode \
         vendor/intel/iwlwifi/iwlwifi-6000g2b-5.ucode:system/etc/firmware/iwlwifi-6000g2b-5.ucode \
         vendor/intel/iwlwifi/iwlwifi-6000g2b-6.ucode:system/etc/firmware/iwlwifi-6000g2b-6.ucode \
